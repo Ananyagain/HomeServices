094af719e3ad320b2f47ad4791798543f18cb43c
AnanyaGain <ananyagain4@gmail.com>
2023-06-08 01:12:01 +0530

    next commit

--- contactform.php
+++ contactform.php
@@ -13,7 +13,7 @@ if(isset($_REQUEST['submit'])) {
  $email = $_REQUEST['email'];
  $message = $_REQUEST['message'];
 
- $mailTo = "ananyagain4@gmail.com";
+ $mailTo = "work@gmail.com";
  $headers = "From: ". $email;
  $txt = "You have received an email from ". $name. ".\n\n".$message;
  mail($mailTo, $subject, $txt, $headers);
